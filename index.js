2a4d24885618391a467d0a0a213afefd86067cf3
Milton Chithenga <miltonachithenga@gmail.com>
2024-09-24 14:35:54 +0200

    Added CORS to handle cross-origin requests

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
+import cors from 'cors';
+
 dotenv.config();
 
 mongoose
@@ -17,30 +19,38 @@ mongoose
     console.log(err);
   });
 
-  const __dirname = path.resolve();
+const __dirname = path.resolve();
 
 const app = express();
 
-app.use(express.json());
+// Enable CORS for all routes
+app.use(cors({
+  origin: 'https://smartvillageshub.com',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+}));
 
+app.use(express.json());
 app.use(cookieParser());
 
-const port=process.env.PORT
+const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
-  console.log('Server is running on port 3000!');
+  console.log(`Server is running on port ${port}!`);
 });
 
 app.use('/backend/user', userRouter);
 app.use('/backend/auth', authRouter);
 app.use('/backend/listing', listingRouter);
 
-
+// Serve static files from the 'client/dist' folder
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-})
+});
 
+// Error handling middleware
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
